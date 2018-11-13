54a215e37f43617302ba5962a8282b14f02042dd
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-13 16:57:19 -0500

    bottom component and css

---
+++ src/components/Bottom.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+class Bottom extends Component
+{
+    render()
+    {
+        return (<div className='bottom'>
+
+                    <div className='bottom-bar'>
+
+                        <div className='bottom-lefthand-side'>
+                            <div>About Us</div>
+                            <div>Contact Us</div>
+                            <div>Careers</div>
+                        </div>
+
+                        <div className='bottom-righthand-side'>
+                           <div>Share on Social Media</div>
+                           <img src='/rawImages/facebook.jpg'></img> {/*get actual images and proper endings.jpg>*/}
+                           <img src='/rawImages/instagram.jpg'></img>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+        )
+    }
+}
+
+function mapStateToProps(state)
+{
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(Header);
