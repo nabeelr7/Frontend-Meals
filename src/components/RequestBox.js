ec3d91a9368fbcd44e0a91c173fad6fc31d45aa5
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-14 11:56:27 -0500

    added files

---
+++ src/components/RequestBox.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+
+class RequestBox extends Component
+{
+    render()
+    {
+        return 
+        
+    }
+}
+
+
+let connectedRequestBox = connect()(RequestBox)
+export default connectedRequestBox;
