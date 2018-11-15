b1ed90f78de3cc21fc4428aa3d528e741446c230
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-14 20:26:29 -0500

    ChefProfile, check if working

--- src/components/ChefProfile.js
+++ src/components/ChefProfile.js
@@ -5,14 +5,34 @@ import { connect } from 'react-redux';
 class ChefProfile extends Component{
     constructor(){
         super();
-        this.state={}
+        this.state={
+            profile: ''
+        }
     }
-
+    componentDidMount(){
+        fetch('/getprofile', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({userName: this.props.userName})
+        }).then((x)=>x.text())
+        .then((response)=>{
+        let parsed=JSON.parse(response)
+        this.setState({profile: parsed})})
+    }
     render(){
-        <div>
-            <div> Chef Info </div>
+        if (!profile){return <div>Loading..</div>}
+        else
+        {return(<div>
+            <img className='chefProfilePic' alt="profilePic" src = {this.props.image}></img>
+            <div>{this.profile.userName}</div>
+            <br/>
+            <div>{this.profile.bio}</div>
+            
             
         </div>
+        )}
     }
 }
 
