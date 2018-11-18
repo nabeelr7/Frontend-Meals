c8eaaa8e6e428a00cf8feb325c0434328f4e6ca7
Nabeel R <nabeelramzeen@gmail.com>
2018-11-17 19:37:35 -0500

    reqs set up

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -8,6 +8,7 @@ class Requests extends Component {
             requests: [],
             filteredRequests: {}
         }
+        this.mapItem = this.mapItem.bind(this)
     }
     componentDidMount() {
         fetch('/getrequests', {
@@ -21,11 +22,10 @@ class Requests extends Component {
             .then((response) => {
                 let parsed = JSON.parse(response)
                 if (!parsed.success) {
-                    this.setState({ foundResults: false })
+                    this.setState({ foundResults: true })
                     return
                 }
                 this.setState({ requests: parsed.result })
-                console.log(this.state)
                 let unanswered = this.state.requests.filter(item => item.requestStatus === 0)
                 let accepted = this.state.requests.filter(item => item.requestStatus === 1)
                 let declined = this.state.requests.filter(item => item.requestStatus === 2)
@@ -41,13 +41,18 @@ class Requests extends Component {
                         }
                     })
                 }
+                console.log(this.state)
+
             })
     }
     mapItem(array) {
-        array.map((item) => {
+        console.log(array)
+        return array.map((item) => {
             return (
                 <div className='req'>
                     <div>{item.mealTitle}</div>
+                    <div>Qty: {item.quantity}</div>
+                    <div>For {item.userName}</div>
                 </div>
             )
         })
@@ -56,7 +61,7 @@ class Requests extends Component {
 
     render() {
         if (!this.state.requests) { return (<div>Loading...</div>) }
-        if (!this.state.foundResults) { return (<div>No Orders Made</div>) }
+        if (this.state.foundResults) { return (<div>No Orders Made</div>) }
         if (this.props.userType === 'chef' && this.state.filteredRequests) {
             return (
                 <div className='req-container'>
@@ -74,7 +79,7 @@ class Requests extends Component {
                     </div>
                     <div className='paid'>
                         <div>Paid</div>
-                        
+
                         {this.state.filteredRequests.paid && this.mapItem(this.state.filteredRequests.paid)}
 
                     </div>
@@ -84,27 +89,28 @@ class Requests extends Component {
         if (this.props.userType === 'client' && this.state.requests) {
             return (
                 <div className='req-container'>
+                    <div>My Orders</div>
                     <div className='unanswered'>
                         <div>Unanswered Requests</div>
-                        
+
                         {this.state.filteredRequests.unanswered && this.mapItem(this.state.filteredRequests.unanswered)}
 
                     </div>
                     <div className='accepted'>
                         <div>Accepted Requests</div>
-                        
+
                         {this.state.filteredRequests.accepted && this.mapItem(this.state.filteredRequests.accepted)}
 
                     </div>
                     <div className='paid'>
                         <div>Paid</div>
-                        
+
                         {this.state.filteredRequests.paid && this.mapItem(this.state.filteredRequests.paid)}
 
                     </div>
                     <div className='declined'>
                         <div>Declined Requests</div>
-                        
+
                         {this.state.filteredRequests.declined && this.mapItem(this.state.filteredRequests.declined)}
 
                     </div>
